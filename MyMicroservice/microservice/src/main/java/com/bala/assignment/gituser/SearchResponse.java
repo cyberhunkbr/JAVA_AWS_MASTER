639df569129d584517521d5b251b93ab7cb79b44
bala gongale <39187198+cyberhunkbr@users.noreply.github.com>
2018-05-28 11:53:28 +0530

    updated code to handle null

--- MyMicroservice/microservice/src/main/java/com/bala/assignment/gituser/SearchResponse.java
+++ MyMicroservice/microservice/src/main/java/com/bala/assignment/gituser/SearchResponse.java
@@ -2,11 +2,13 @@ package com.bala.assignment.gituser;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SearchResponse {
 	
 	@JsonProperty("items")
+	@JsonInclude(JsonInclude.Include.NON_NULL) 
 	private List<Users> users;
 	
 	public List<Users> getUsers() {
@@ -16,6 +18,7 @@ public class SearchResponse {
 		this.users = users;
 	}
 	
+	@JsonInclude(JsonInclude.Include.NON_NULL) 
 	String errorText ;
 
 	public String getErrorText() {
